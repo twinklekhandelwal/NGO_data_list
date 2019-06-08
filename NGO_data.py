097d1a00bed8fc867b0dbe1a43db378d7b46ea62
twinkle <you@example.com>
2019-06-08 16:32:39 +0530

    update second task

--- NGO_data.py
+++ NGO_data.py
@@ -10,13 +10,13 @@ soup = BeautifulSoup(url1.text,"html.parser")
 main_data=soup.find("div",class_="container")
 table_data=main_data.find("table",class_="jsx-697282504 certified-ngo-table")
 
-td1=table_data.find_all("tr",class_="jsx-697282504")
-td2=table_data.find_all("td",class_="jsx-697282504 nonprofit-name-desktop")
+tr=table_data.find_all("tr",class_="jsx-697282504")
+td=table_data.find_all("td",class_="jsx-697282504 nonprofit-name-desktop")
 def NGO_full_list(): 
     list1=[]
     list2=[]
     list3=[]
-    for i in td2:
+    for i in td:
         dic={}
         Name_list=i.find("div",class_="col")
         Name_list_data=Name_list.get_text()
@@ -35,10 +35,30 @@ def NGO_full_list():
         m1=cause_list[z]
         dic2={}
         d1=m1.get_text()
+        
         list3.append(d1)
     dic["city"]=list3
     return dic
-dic_list=NGO_full_list()
-pprint.pprint(dic_list) 
-
+dic_list=NGO_full_list() 
+def arrage_name(data):
+    dic1={}
+    new_city_list=[]
+    Name_list=data["name"]
+    City_list=data["city"]
+    
+    for i in City_list:
+        
+        if i not in new_city_list:
+            new_city_list.append(i)
+    for j in new_city_list:
+        name1=[]
+        z=0
+        for k in City_list:
+            if j  in  k:
+                m=Name_list[z]
+                name1.append(m) 
+            z=z+1
+        dic1[j]=name1
+    pprint.pprint (dic1)               
+arrage_name(dic_list)    
 
